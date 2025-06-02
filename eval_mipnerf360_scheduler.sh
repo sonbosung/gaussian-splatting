590758136d5ab7a7d439171397b35a5b37c485ca
sonbosung <sonbosung@kakao.com>
2025-06-02 09:33:02 +0900

    minor changes

--- eval_mipnerf360_scheduler.sh
+++ eval_mipnerf360_scheduler.sh
@@ -3,7 +3,7 @@ exp_path=${diskpath}/auggs/experiments
 colmap_path=${diskpath}/360
 colmap_path_augmented=${diskpath}/360_augmented
 # for scene in bicycle flowers garden stump treehill
-for scene in flowers garden stump treehill
+for scene in garden treehill
 do
     
     python train_scheduler.py -s ${colmap_path_augmented}/${scene} -m ${exp_path}/360_augmented_scheduler/${scene} \
@@ -20,7 +20,7 @@ do
     python metrics.py -m ${exp_path}/360_augmented_scheduler/${scene}
 done
 
-for scene in bonsai counter kitchen room
+for scene in bonsai kitchen
 do
     python train_scheduler.py -s ${colmap_path_augmented}/${scene} -m ${exp_path}/360_augmented_scheduler/${scene} \
     -i images_2 \
